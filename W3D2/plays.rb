1b9824bd43e0f24f4fe0430f10375bca91ce890f
Charlie Thomas <cjthom03@gmail.com>
2018-04-29 20:27:27 -0700

    Add ::find_by_tite and ::find_by_playwright to Play class

--- W3D2/plays.rb
+++ W3D2/plays.rb
@@ -19,6 +19,32 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    data = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        plays.title LIKE ?
+      SQL
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def self.find_by_playwright(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        plays
+      JOIN
+        playwrights ON plays.playwright_id = playwrights.id
+      WHERE
+        playwrights.name LIKE ?
+    SQL
+    data.map { |datum| Play.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -47,5 +73,28 @@ class Play
       WHERE
         id = ?
     SQL
+  end
+end
+
+class Playwright
+  def self.all
+  end
+
+  def self.find_by_name(name)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def get_plays
   end
 end
