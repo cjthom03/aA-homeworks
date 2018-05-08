9a18d0b92d40fec70f9d37ebe31d40524116c0c6
Charlie Thomas <cjthom03@gmail.com>
2018-05-07 21:44:49 -0700

    complete Rails Auth project

--- W4D3/RailsAuth/app/models/user.rb
+++ W4D3/RailsAuth/app/models/user.rb
@@ -13,9 +13,10 @@
 class User < ApplicationRecord
   attr_reader :password
 
+  before_validation :ensure_session_token
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: {message: "Password can't be blank"}
-  validates :password, length: {minium: 6, allow_nil: true}
+  validates :password, length: {minimum: 6, allow_nil: true}
 
   def password=(password)
     @password = password
@@ -24,5 +25,28 @@ class User < ApplicationRecord
 
   def is_password?(password)
     BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token #return the new token
+  end
+
+  def ensure_session_token
+    # we must be sure to use the ||= operator instead of = or ||, otherwise
+    # we will end up with a new session token every time we create
+    # a new instance of the User class. This includes finding it in the DB!
+    self.session_token ||= self.class.generate_session_token
   end
 end
