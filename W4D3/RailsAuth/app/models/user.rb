0ba8e4e3f8b75388d22ea2ff386ea02dff8322d8
Charlie Thomas <cjthom03@gmail.com>
2018-05-07 21:24:47 -0700

    Initial Commit RailsAuth Project

---
+++ W4D3/RailsAuth/app/models/user.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: {message: "Password can't be blank"}
+  validates :password, length: {minium: 6, allow_nil: true}
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+end
