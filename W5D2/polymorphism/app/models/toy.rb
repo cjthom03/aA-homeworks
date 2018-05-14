70aeb6ca671f5f43f0c52167c92396703ea87012
Charlie Thomas <cjthom03@gmail.com>
2018-05-13 17:47:43 -0700

    Add polymorphic toy model and associations

--- W5D2/polymorphism/app/models/toy.rb
+++ W5D2/polymorphism/app/models/toy.rb
@@ -1,4 +1,19 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :bigint(8)        not null, primary key
+#  name         :string           not null
+#  toyable_type :string
+#  toyable_id   :bigint(8)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Toy < ApplicationRecord
+  validates :name, presence: true
   validates :name, uniqueness: { scope: [:toyable] }
   belongs_to :toyable, polymorphic: true
+
+
 end
