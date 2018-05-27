d9ae42b3f2d55da12b654685840e36350c3978a0
Charlie Thomas <cjthom03@gmail.com>
2018-05-27 11:57:05 -0700

    Complete redux jobListing

--- W7D1/Redux_JobListing/frontend/reducer.js
+++ W7D1/Redux_JobListing/frontend/reducer.js
@@ -1,10 +1,21 @@
 const initialState = {
-  city: "Please Select", 
+  city: "Please Select",
   jobs: []
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  Object.freeze(state);
+  switch(action.type) {
+    case("SWITCH_LOCATION"):
+      const newState = {
+        city: action.city,
+        jobs: action.jobs
+      };
+      return newState;
+    default:
+      return state;
+  }
 };
+
 
 export default reducer;
