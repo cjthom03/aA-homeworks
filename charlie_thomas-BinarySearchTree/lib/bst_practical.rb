96d8c8b19b29d7e398ce2314294e8332e5e3e760
Charlie Thomas <cjthom03@gmail.com>
2018-07-09 11:01:44 -0700

    Update kth largest function to correct implementation

--- charlie_thomas-BinarySearchTree/lib/bst_practical.rb
+++ charlie_thomas-BinarySearchTree/lib/bst_practical.rb
@@ -1,12 +1,28 @@
 require_relative 'binary_search_tree'
 
-def kth_largest(tree_node, k)
-  tree_queue = [tree_node]
-  tree_queue.each do |node|
-    break if tree_queue.length == k
-    tree_queue << node.left if node.left
-    tree_queue << node.right if node.right
-  end
+# THIS APPROACH IS WRONG, this is not what the question was asking for
 
-  tree_queue[k]
+# def kth_largest(tree_node, k)
+#   tree_queue = [tree_node]
+#   tree_queue.each do |node|
+#     break if tree_queue.length == k
+#     tree_queue << node.left if node.left
+#     tree_queue << node.right if node.right
+#   end
+#
+#   tree_queue[k]
+# end
+
+
+def kth_largest(tree_node, k)
+  reverse_order_traversal(tree_node, [], k).first
+end
+
+
+def reverse_order_traversal(node = @root, arr = [], k)
+  reverse_order_traversal(node.right, arr, k) if node.right && arr.length != k
+  arr.unshift(node) if arr.length != k
+  reverse_order_traversal(node.left, arr, k) if node.left && arr.length != k
+
+  arr
 end
