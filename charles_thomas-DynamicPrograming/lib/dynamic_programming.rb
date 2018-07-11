ba12c558ec167d9dae4dafe65853eb3d1d242ae1
Charlie Thomas <cjthom03@gmail.com>
2018-07-11 14:07:35 -0700

    Start dynmaic programming

---
+++ charles_thomas-DynamicPrograming/lib/dynamic_programming.rb
@@ -0,0 +1,115 @@
+require 'byebug'
+
+class DynamicProgramming
+
+  def initialize
+    @blair_cache = {1 => 1, 2 => 2}
+    @frog_cache = {
+      1 => [[1]],
+      2 => [[1,1], [2]],
+      3 => [[1,2], [1,1,1], [2,1], [3]]
+    }
+    @super_frog_cache = { 0 => [[]]}
+  end
+
+  def blair_nums(n)
+    return @blair_cache[n] if @blair_cache[n]
+    kth_odd_num = ((n-1) * 2 ) - 1
+    ans = blair_nums(n-1) + blair_nums(n-2) + kth_odd_num
+    @blair_cache[n] = ans
+  end
+
+  def blair_nums_bottoms_up(n)
+    cache = {1 => 1, 2 => 2}
+    return nil if n < 1
+    return cache[n] if n < 3
+
+    (3..n).each do |i|
+      kth_odd_num = ((i-1) * 2 ) - 1
+      cache[i] = cache[i-1] + cache[i-2] + kth_odd_num
+    end
+
+    return cache[n]
+  end
+
+  def frog_hops_bottom_up(n)
+    cache = frog_cache_builder(n)
+    cache[n]
+  end
+
+  def frog_cache_builder(n)
+    return {} if n < 1
+    cache = {
+      1 => [[1]],
+      2 => [[1,1], [2]],
+      3 => [[1,2], [1,1,1], [2,1], [3]]
+    }
+
+    (4..n).each do |i|
+      plus3 = cache[i-3].map { |steps| steps + [3] }
+      plus2 = cache[i-2].map { |steps| steps + [2] }
+      plus1 = cache[i-1].map { |steps| steps + [1] }
+      cache[i] = plus3 + plus2 + plus1
+    end
+
+    cache
+  end
+
+  def frog_hops_top_down(n)
+    frog_hops_top_down_helper(n)
+    @frog_cache[n]
+  end
+
+  def frog_hops_top_down_helper(n)
+    return @frog_cache[n] if @frog_cache[n]
+
+    plus3 = frog_hops_top_down_helper(n-3).map { |steps| steps + [3] }
+    plus2 = frog_hops_top_down_helper(n-2).map { |steps| steps + [2] }
+    plus1 = frog_hops_top_down_helper(n-1).map { |steps| steps + [1] }
+
+    ans = plus3 + plus2 + plus1
+    @frog_cache[n] = ans
+  end
+
+  def super_frog_hops(n, k)
+    @super_frog_cache = { 0 => [[]]}
+    super_frog_helper(n, k)
+    @super_frog_cache[n]
+  end
+
+  def super_frog_helper(n, k)
+    return @super_frog_cache[n] if @super_frog_cache[n]
+
+    unless @super_frog_cache[k]
+      (1..k).each do |max_step|
+        solutions = []
+        max_step.downto(1).each do |step|
+          solutions += @super_frog_cache[max_step - step].map {|el| el + [step]}
+        end
+
+        @super_frog_cache[max_step] = solutions
+        return @super_frog_cache[n] if @super_frog_cache[n]
+      end
+    end
+
+    ans = []
+    k.downto(1).each do |level|
+      ans += super_frog_helper(n-level, k).map {|steps| steps + [level]}
+    end
+
+    @super_frog_cache[n] = ans
+  end
+
+
+  def knapsack(weights, values, capacity)
+
+  end
+
+  # Helper method for bottom-up implementation
+  def knapsack_table(weights, values, capacity)
+
+  end
+
+  def maze_solver(maze, start_pos, end_pos)
+  end
+end
